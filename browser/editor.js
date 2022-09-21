e60893f15d5ab57e6fd08e2db85ddbb74f13ac16
bem130 <haruki070617@gmail.com>
2022-09-21 20:05:32 +0900

    remake the cursor

--- browser/editor.js
+++ browser/editor.js
@@ -14,47 +14,47 @@ class NEditorUI {
         this.editor = new NEditor();
         this.ui = editorarea;
         this.initalui();
-        this.selector();
     }
     initalui() {
         this.ui.classList.add("NEditor");
         // edit area
-        let editarea = document.createElement("div");
-        editarea.classList.add("editarea");
-        this.ui.appendChild(editarea);
-        {
-            for (let i=0;i<this.editor.lines.length;i++) {
-                let lines = document.createElement("div");
-                lines.classList.add("line");
-                for (let j=0;j<this.editor.lines[i].length;j++) {
-                    let chars = document.createElement("span");
-                    chars.classList.add("char");
-                    chars.innerText = this.editor.lines[i][j];
-                    lines.appendChild(chars);
-                }
-                editarea.appendChild(lines);
-                console.log(this.editor.lines[i]);
-            }
-        }
+        this.editarea = document.createElement("div");
+        this.editarea.classList.add("editarea");
+        this.ui.appendChild(this.editarea);
+        this.updatelines();
+        // cursor
+        this.addtext = document.createElement("input");
+        this.addtext.classList.add("addtext");
         // under bar
         let infobar = document.createElement("div");
         infobar.classList.add("infobar");
         {
-            // cursor
+            // cursor info
             let infocursor = document.createElement("div");
             infocursor.innerHTML = this.editor.cursor;
             infobar.appendChild(infocursor);
         }
         this.ui.appendChild(infobar);
+        this.moveselector();
     }
-    selector() {
-        let char = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]}) > span:nth-child(${this.editor.cursor[1]})`);
-        {
-            let cursor = document.createElement("div");
-            cursor.classList.add("cursor");
-            char.after(cursor);
+    updatelines() {
+        this.editarea.innerHTML = "";
+        for (let i=0;i<this.editor.lines.length;i++) {
+            let lines = document.createElement("div");
+            lines.classList.add("line");
+            for (let j=0;j<this.editor.lines[i].length;j++) {
+                let chars = document.createElement("span");
+                chars.classList.add("char");
+                chars.innerText = this.editor.lines[i][j];
+                lines.appendChild(chars);
+            }
+            this.editarea.appendChild(lines);
         }
-        console.log(char);
+    }
+    moveselector() {
+        let char = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]}) > span:nth-child(${this.editor.cursor[1]})`);
+        char.after(this.addtext);
+        this.addtext.focus();
     }
     setui() {}
 }
