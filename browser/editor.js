edcc71e916b253731ec54e9c220680a108519192
bem130 <haruki070617@gmail.com>
2022-09-23 23:22:47 +0900

    change about the caret

--- browser/editor.js
+++ browser/editor.js
@@ -1,21 +1,23 @@
 class NEditor {
     constructor() {
-        this.lines = [""];
-        this.cursor = [1,1];
+        this.text = [""];
+        this.cursor = 1;
     }
     setText(text) {}
     addText(text) {
-        this.lines[this.cursor[0]-1] = this.lines[this.cursor[0]-1].slice(0,this.cursor[1]-1)+text+this.lines[this.cursor[0]-1].slice(this.cursor[1]-1);
-        this.cursor[1]++;
+        this.text = this.text.slice(0,this.cursor-1)+text+this.text.slice(this.cursor-1);
+        this.cursor++;
     }
     deleteText() {
-        if (this.cursor[1]>0) {
-            this.lines[this.cursor[0]-1] = this.lines[this.cursor[0]-1].slice(0,this.cursor[1]-2)+this.lines[this.cursor[0]-1].slice(this.cursor[1]-1);
-            this.cursor[1]--;
+        if (this.cursor>1) {
+            this.text = this.text.slice(0,this.cursor-2)+this.text.slice(this.cursor-1);
+            this.cursor--;
         }
     }
-    moveCursor(l,c) {
-        this.cursor[1]+=c;
+    moveCursor(c) {
+        if (this.cursor+c>0&&this.cursor+c<this.text.length+2) {
+            this.cursor+=c;
+        }
     }
     selectText(text) {}
 }
@@ -39,14 +41,14 @@ class NEditorUI {
         this.addtextarea.classList.add("addtext");
         //this.addtextarea.onchange = this.addtext.bind(this);
         this.addtextarea.oninput = this.addtext.bind(this);
-        this.addtextarea.onkeydown = this.keyevent.bind(this);
+        this.editarea.onkeydown = this.keyevent.bind(this);
         // under bar
         let infobar = document.createElement("div");
         infobar.classList.add("infobar");
         {
             // cursor info
             this.infocursor = document.createElement("div");
-            this.infocursor.innerHTML = [this.editor.cursor[0],this.editor.cursor[1]-1];
+            this.infocursor.innerHTML = [this.editor.cursor,this.editor.cursor-1];
             infobar.appendChild(this.infocursor);
         }
         this.ui.appendChild(infobar);
@@ -55,7 +57,7 @@ class NEditorUI {
     addtext(e) {
         if( e.isComposing ) {
             this.composingtext.innerText = e.target.value;
-            this.infocursor.innerText = [this.editor.cursor[0],this.editor.cursor[1]+e.target.value.length];
+            this.infocursor.innerText = [this.editor.cursor+e.target.value.length];
             return;
         }
         else {
@@ -74,49 +76,60 @@ class NEditorUI {
             this.updatelines();
         }
         if (e.keyCode==37) {
-            this.editor.moveCursor(0,-1);
+            this.editor.moveCursor(-1);
             this.updatelines();
         }
         if (e.keyCode==39) {
-            this.editor.moveCursor(0,1);
+            this.editor.moveCursor(1);
             this.updatelines();
         }
     }
     updatelines() {
         this.editarea.innerHTML = "";
-        for (let i=0;i<this.editor.lines.length;i++) {
-            let lines = document.createElement("div");
-            lines.classList.add("line");
-            for (let j=0;j<this.editor.lines[i].length;j++) {
+        let lines = document.createElement("div");
+        let i = 0;
+        for (i=0;i<=this.editor.text.length;i++) {
+            if (i+1==this.editor.cursor) {
+                lines.appendChild(this.composingtext);
+                lines.appendChild(this.addtextarea);
+            }
+            if (this.editor.text[i]==null) {
+                lines.classList.add("line");
+                this.editarea.appendChild(lines);
+            }
+            else if (this.editor.text[i]=="\n") {
+                lines.classList.add("line");
+                this.editarea.appendChild(lines);
+                lines = document.createElement("div");
+            }
+            else {
                 let chars = document.createElement("span");
                 chars.classList.add("char");
-                chars.innerText = this.editor.lines[i][j];
+                chars.innerText = this.editor.text[i];
                 lines.appendChild(chars);
             }
-            this.editarea.appendChild(lines);
         }
-        this.moveselector();
+        this.infocursor.innerText = this.editor.cursor;
+        this.addtextarea.focus();
     }
     moveselector() {
-        this.infocursor.innerText = this.editor.cursor;
-        let chars = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]}) > span:nth-child(${this.editor.cursor[1]-1})`);
+        let chars = document.querySelector(`.NEditor > div.editarea span:nth-child(${this.editor.cursor-1})`);
         if (chars!=null) {
             chars.after(this.addtextarea);
             chars.after(this.composingtext);
         }
         else {
-            chars = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]}) > span:nth-child(${this.editor.cursor[1]})`);
+            chars = document.querySelector(`.NEditor > div.editarea span:nth-child(${this.editor.cursor})`);
             if (chars!=null) {
                 chars.before(this.addtextarea);
                 chars.before(this.composingtext);
             }
             else {
-                chars = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]})`);
-                chars.after(this.addtextarea);
-                chars.after(this.composingtext);
+                chars = document.querySelector(`.NEditor > div.editarea`);
+                chars.appendChild(this.addtextarea);
+                chars.appendChild(this.composingtext);
             }
         }
-        this.addtextarea.focus();
     }
     setui() {}
 }
