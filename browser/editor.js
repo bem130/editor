66df39274546b6cb9e00aaddd2903e05113e4f56
bem130 <haruki070617@gmail.com>
2022-09-23 19:27:10 +0900

    make japanese input

--- browser/editor.js
+++ browser/editor.js
@@ -1,10 +1,19 @@
 class NEditor {
     constructor() {
-        this.lines = ["Hello World!","This is a editor for web-apps"];
-        this.cursor = [1,5];
+        this.lines = [""];
+        this.cursor = [1,0];
+        this.composing = "";
+        this.composingcursor = 0;
     }
     setText(text) {}
-    addText(text) {}
+    composingText(text) {
+        this.composingtext = text;
+        this.composingcursor = text.length;
+    }
+    addText(text) {
+        this.lines[this.cursor[0]-1] = this.lines[this.cursor[0]-1].slice(0,this.cursor[1])+text+this.lines[this.cursor[0]-1].slice(this.cursor[1]);
+        this.cursor[1]++;
+    }
     deleteText(text) {}
     moveCursor(x,y) {}
     selectText(text) {}
@@ -20,21 +29,40 @@ class NEditorUI {
         // edit area
         this.editarea = document.createElement("div");
         this.editarea.classList.add("editarea");
+        this.editarea.onclick = function() {this.addtextarea.focus()}.bind(this);
         this.ui.appendChild(this.editarea);
         this.updatelines();
         // cursor
-        this.addtext = document.createElement("input");
-        this.addtext.classList.add("addtext");
+        this.addtextarea = document.createElement("textarea");
+        this.addtextarea.classList.add("addtext");
+        //this.addtextarea.onchange = this.addtext.bind(this);
+        this.addtextarea.oninput = this.addtext.bind(this);
         // under bar
         let infobar = document.createElement("div");
         infobar.classList.add("infobar");
         {
             // cursor info
             let infocursor = document.createElement("div");
-            infocursor.innerHTML = this.editor.cursor;
+            infocursor.innerHTML = [this.editor.cursor[0],this.editor.cursor[1]];
             infobar.appendChild(infocursor);
         }
         this.ui.appendChild(infobar);
+        this.moveselector();
+    }
+    addtext(e) {
+        if( e.isComposing ) {
+          console.log("未確定",e.target.value);
+          this.editor.composingText(e.target.value);
+          return
+        }
+        else {
+            console.log("確定");
+            for (let i=0;i<e.target.value.length;i++) {
+                this.editor.addText(e.target.value[i]);
+            }
+            e.target.value = "";
+        }
+        this.updatelines();
         this.moveselector();
     }
     updatelines() {
@@ -52,9 +80,13 @@ class NEditorUI {
         }
     }
     moveselector() {
-        let char = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]}) > span:nth-child(${this.editor.cursor[1]})`);
-        char.after(this.addtext);
-        this.addtext.focus();
+        console.log(this.editor.cursor)
+        let chars = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]}) > span:nth-child(${this.editor.cursor[1]})`);
+        if (chars==null) {
+            chars = document.querySelector(`.NEditor > div.editarea > div:nth-child(${this.editor.cursor[0]})`);
+        }
+        chars.after(this.addtextarea);
+        this.addtextarea.focus();
     }
     setui() {}
 }
